2d6f5caa0df7b40c3aaa749953ee8707ece3fad7
robert schurig <rsc@mi24.tv>
2022-04-09 23:38:07 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import './index.css';
 
-import { App } from 'components/app';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { App } from 'pages/app';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
